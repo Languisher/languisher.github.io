bc2b7c219a1bfbe5f3a5eddfba3bacdca54a2e28
Languisher <lns_brandon@qq.com>
2024-08-06 22:40:21 +0800

    domain changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://languisher.github.io',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
